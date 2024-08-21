aac13295d388d6e50c7511b1914e976dfc7a1edb
romani006 <romaninanli18@gmail.com>
2024-08-21 19:09:54 +0100

    Length modified
    Limited the length of characters you can make your username to 20

--- namelist.py
+++ namelist.py
@@ -3,6 +3,8 @@ while True:
     name = input("Enter a name: ")
     if usernames.count(name) >= 1:
         print("Username already in use!")
+    elif len(name) > 20:
+        print("Username has too many characters!")
     else:
         print("Username created!")
         usernames.append(name)
