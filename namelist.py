e120a9c060396e56990895131a3f40c35eb9a5fc
romani006 <romaninanli18@gmail.com>
2024-08-28 19:27:46 +0100

    Modified the condition
    Changed the condition which checked whether the username had been taken or not to a more convenient one, also changed when the username was actually made

--- namelist.py
+++ namelist.py
@@ -1,12 +1,12 @@
 usernames = []
 while True:
     name = input("Enter a name: ")
-    if usernames.count(name) >= 1:
+    if name in usernames:
         print("Username already in use!")
     elif len(name) > 20:
         print("Username has too many characters!")
     else:
-        print("Username created!")
         usernames.append(name)
+        print("Username created!")
 
 
