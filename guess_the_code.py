8606da220e4318015e626dc26750c0bfee8b764e
romani006 <romaninanli18@gmail.com>
2024-09-09 20:59:47 +0100

    Removed comments as they made the code less readable.

--- guess_the_code.py
+++ guess_the_code.py
@@ -1,28 +1,25 @@
-import random  # allows for probability
+import random
 
 random_code = [random.randint(0, 9), random.randint(0, 9), random.randint(0, 9)]
-# ^ creates random number for each digit of 3 digit code
 random_code = str(random_code[0]) + str(random_code[1]) + str(random_code[2])
-# converts all values to string for comparison
 
-number_of_guesses = 0  # tally of how many guesses they user has taken
+number_of_guesses = 0
 
-while True:  # loops until user correctly guesses code
+while True:
     correct_numbers = 0
-    user_guess = input("Guess the code! Enter a 3 digit number: ")  # user inputs their guess
-    number_of_guesses += 1  # adds a guess to the value
-    if len(user_guess) != 3:  # if input was not 3 digits long
-        print("Invalid guess length, try again!")  # return to start of loop
-    else:  # if they
-        if user_guess[0] == random_code[0]:  # if user got first digit correct
-            correct_numbers += 1  # add to value
-        if user_guess[1] == random_code[1]:  # if user got second digit correct
-            correct_numbers += 1  # add to value
-        if user_guess[2] == random_code[2]:  # if user got third digit correct
-            correct_numbers += 1  # add to value
-        if correct_numbers == 3:  # if the user guessed all the digits correct then
+    user_guess = input("Guess the code! Enter a 3 digit number: ")
+    number_of_guesses += 1
+    if len(user_guess) != 3:
+        print("Invalid guess length, try again!")
+    else:
+        if user_guess[0] == random_code[0]:
+            correct_numbers += 1
+        if user_guess[1] == random_code[1]:
+            correct_numbers += 1
+        if user_guess[2] == random_code[2]:
+            correct_numbers += 1
+        if correct_numbers == 3:
             print(f"You guessed the code! You guessed the number {random_code} in {number_of_guesses} guesses!")
-            # ^ paste guesses and real code
-            break  # user guessed code, breaks from loop
-        else:  # if they didn't get the code correct
-            print(f"{correct_numbers} numbers correct, try again!")  # user got it wrong, loops back to start
+            break
+        else:
+            print(f"{correct_numbers} numbers correct, try again!")
